274b01e774197ab8f8b5ae718d66115f9749be1c
ice6504 <aphinan6504@gmail.com>
2024-04-17 23:29:13 +0700

    Add Info

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import Drawer from "./components/Drawer/Drawer";
 import Header from "./components/Header/Header";
 import Intro from "./components/Intro/Intro";
+import Info from "./components/Info/Info";
+
+// Img
+import happy from "./assets/img/section3/undraw_happy_news_hxmt.svg";
+import social from "./assets/img/section4/undraw_social_influencer_sgsv.svg";
+import mention from "./assets/img/section5/undraw_mention_6k5d.svg";
 
 function App() {
   return (
     <>
       <Drawer>
         <Header />
-        <div className="max-w-screen-xl mx-auto max-xl:px-5">
+        <div className="max-w-screen-xl mx-auto max-xl:px-5 max-sm:space-y-32 md:max-lg:space-y-20 max-lg:pb-5">
           <Intro />
-          <div className="h-screen"></div>
+          <Info img={happy} />
+          <Info img={social} />
+          <Info img={mention} hasBtn={true} btnTitle="Purchase Now" />
         </div>
+        <div className="bg-blue-yonder-200 h-screen"></div>
       </Drawer>
     </>
   );
---
+++ src/components/Info/Info.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+
+function Info(props) {
+  const { img, hasBtn, btnTitle } = props;  
+  console.log(hasBtn);
+  return (
+    <>
+      <div className="min-h-[60vh] xl:h-screen flex max-lg:flex-col justify-center items-center gap-16">
+        <img src={img} alt="" className="w-full sm:w-2/3 lg:w-1/2" />
+        <div className="flex flex-col lg:w-2/5 sm:px-10 gap-5">
+          <h2 className="text-blue-yonder-900 font-medium text-4xl">
+            Light, Fast & Powerful
+          </h2>
+          <p className="text-blue-yonder-400 leading-7 tracking-wide">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus <br />
+            mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
+            sem. Nulla consequat massa quis enim.
+          </p>
+          {hasBtn ? (
+            <button className="btn btn-secondary btn-sm rounded-sm w-36 sm:mt-5">
+              {btnTitle}
+            </button>
+          ) : null}
+        </div>
+      </div>
+    </>
+  );
+}
+
+Info.propTypes = {
+  img: PropTypes.string.isRequired,
+  hasBtn: PropTypes.bool,
+  btnTitle: PropTypes.string,
+};
+
+export default Info;
